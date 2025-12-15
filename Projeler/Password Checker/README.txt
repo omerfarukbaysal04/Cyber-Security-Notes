9a0d64a1b8f67aed510a1d74581b035f64ca6cb6
omerfarukbaysal04 <baysalomerfaruk54@gmail.com>
2025-12-15 16:37:10 +0300

    Update directory

--- Projeler/Password Checker/README.txt
+++
@@ -1,1 +0,0 @@
-exe dosyası dist klasöründedir
--- Python Kodları/otomasyon_kodlari.py
+++
@@ -1,9 +0,0 @@
-import pywhatkit as kit
-
-#pywhatkit komutları-otomasyon
-
-kit.sendwhatmsg_to_group("Grup adı","grup mesaj",10,31) #Whatsapp web üzerinden istenen zamanda mesaj atar.
-kit.playonyt("Video ismi")  # Youtube'dan videoyu direkt oynatır
-kit.info("Atatürk", lines=3)  # Wikipedia'dan bilgi çeker
-kit.send_mail("Konu", "Mesaj", "mailadresi", "sifre", "hedefmailadres")  # E-posta atar
-
